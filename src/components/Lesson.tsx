1f789f8425d4340a5aa190ae76213943121c76e9
Wallysson <wlc.couto@gmail.com>
2022-06-23 22:05:49 -0300

    class 3 finished

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from "phosphor-react";
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
+import { Link } from "react-router-dom";
 
 interface LessonProps {
   title: string;
@@ -18,14 +19,14 @@ export function Lesson(props: LessonProps) {
   )
 
   return (
-    <a href="#">
+    <Link to={`/event/lesson/${props.slug}`} className="group">
       <span
         className="text-gray-300"
       >
         {availableDateFormatted}
       </span>
       
-      <div className="border border-gray-500 rounded p-4 mt-2">
+      <div className="border border-gray-500 rounded p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
 
           { isLessonAvailable ? (
@@ -52,6 +53,6 @@ export function Lesson(props: LessonProps) {
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,27 +1,83 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning, PictureInPicture } from "phosphor-react";
 
-export function Video() {
+import '@vime/core/themes/default.css'
+
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  if(!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId}/>
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">Aula 01 - Abertura</h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis earum aliquid, possimus sequi et nam vero quod eum aperiam non hic doloribus cum velit necessitatibus dolores? Nobis incidunt ullam nulla!</p>
+            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
+            <p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
 
             <div className="flex items-center gap-4 mt-6">
               <img 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/Wallysson.png"
+                src={data.lesson.teacher.avatarURL}
               />
 
               <div className="leading-relaxed">
-                <strong className="font-bold text-1xl block">Wallysson Lima</strong>
-                <span className="text-gray-200 text-sm block">CTO at CEL Consultoria</span>
+                <strong className="font-bold text-1xl block">{data.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
