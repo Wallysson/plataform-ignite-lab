2d087966778455d049e756674a164dde8e7026ef
Wallysson <wlc.couto@gmail.com>
2022-06-25 17:57:03 -0300

    responsividade na subscriber

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -63,26 +63,26 @@ export function Video(props: VideoProps) {
         </div>
       </div>
 
-      <div className="p-8 max-w-[1100px] mx-auto">
-        <div className="flex items-start gap-16">
+      <div className="p-8 max-w-[1100px] mx-auto ">
+        <div className="flex flex-col md:flex-row items-start gap-16 ">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
             <p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
 
-            <div className="flex items-center gap-4 mt-6">
+            <div className="flex items-center gap-4 mt-6 ">
               <img 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
                 src={data.lesson.teacher.avatarURL}
               />
 
-              <div className="leading-relaxed">
+              <div className="leading-relaxed ">
                 <strong className="font-bold text-1xl block">{data.lesson.teacher.name}</strong>
                 <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 ">
             <a href="#" className="bg-green-500 p-4 text-[0.875rem] flex items-center rounded uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24}/>
               Comunidade do Discord
@@ -95,7 +95,7 @@ export function Video(props: VideoProps) {
           </div>
         </div>
 
-        <div className="gap-8 mt-8 grid grid-cols-2">
+        <div className="gap-8 mt-8 grid xl:grid-cols-2 lg:grid-cols-1">
           <a href="" className="bg-gray-700 rounded overflow-hidden flex items-strech gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40}/>
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -33,21 +33,21 @@ export function Subscribe() {
   }
 
   return (
-    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center p-4 overflow-hidden">
+      <div className="w-full max-w-[1100px] flex flex-col lg:flex-row items-center justify-between mt-10 lg:mt-20 mx-auto">
+        <div className="max-w-[640px] flex flex-col items-center lg:block">
           <Logo />
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="mt-8 text-3xl lg:text-[2.5rem] leading-tight text-center lg:text-start">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 text-gray-200 leading-relaxed text-center lg:text-start">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
-        <div className="bg-gray-700 border p-8 rounded">
+        <div className="border bg-gray-700 border-gray-500 p-8 rounded mt-8 lg:mt-0 w-screen sm:w-[70%] md:w-[50%] lg:w-fit">
           <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong>
-          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full">
+          <form onSubmit={handleSubscribe} className="flex flex-col gap-2 w-full ">
             <input
               className="bg-gray-900 rounded px-5 h-14" 
               type="text" 
@@ -71,7 +71,10 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src='./src/assets/code.png' alt="Imagem Código" />
+      <img 
+        className='mt-4 lg:mt-0'
+        src='./src/assets/code.png' alt="Imagem Código" 
+      />
     </div>
   )
 }
