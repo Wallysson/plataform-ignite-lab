56b054c1647397de5cfa8b6b05274f2e247ef09b
Wallysson <wlc.couto@gmail.com>
2022-06-25 18:23:13 -0300

    responsividade no componente video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -66,8 +66,8 @@ export function Video(props: VideoProps) {
       <div className="p-8 max-w-[1100px] mx-auto ">
         <div className="flex flex-col md:flex-row items-start gap-16 ">
           <div className="flex-1">
-            <h1 className="text-2xl font-bold">{data.lesson.title}</h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
+            <h1 className="text-lg md:text-2xl font-bold">{data.lesson.title}</h1>
+            <p className="text-sm md:text-base mt-4 text-gray-200 leading-relaxed">{data.lesson.description}</p>
 
             <div className="flex items-center gap-4 mt-6 ">
               <img 
@@ -76,13 +76,13 @@ export function Video(props: VideoProps) {
               />
 
               <div className="leading-relaxed ">
-                <strong className="font-bold text-1xl block">{data.lesson.teacher.name}</strong>
+                <strong className="font-bold text-lg md:text-2xl block">{data.lesson.teacher.name}</strong>
                 <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
 
-          <div className="flex flex-col gap-4 ">
+          <div className="flex flex-col gap-4 w-full md:w-fit ">
             <a href="#" className="bg-green-500 p-4 text-[0.875rem] flex items-center rounded uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
               <DiscordLogo size={24}/>
               Comunidade do Discord
@@ -95,27 +95,27 @@ export function Video(props: VideoProps) {
           </div>
         </div>
 
-        <div className="gap-8 mt-8 grid xl:grid-cols-2 lg:grid-cols-1">
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-strech gap-6 hover:bg-gray-600 transition-colors">
+        <div className="gap-8 mt-8 grid md:grid-cols-2">
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-strech gap-2 md:gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40}/>
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-1xl">Material complementar</strong>
-              <p className="text-sm text-gray-200 mt-2">Acesse o material complementar para acelerar seu desenvolvimento</p>
+              <strong className="text-lg md:text-1xl">Material complementar</strong>
+              <p className="text-xs lg:text-sm text-gray-200 mt-2">Acesse o material complementar para acelerar seu desenvolvimento</p>
             </div>
             <div className="h-full p-6 flex items-center">
               <CaretRight size={24}/>
             </div>
           </a>
 
-          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-strech gap-6 hover:bg-gray-600 transition-colors">
+          <a href="" className="bg-gray-700 rounded overflow-hidden flex items-strech gap-2 md:gap-6 hover:bg-gray-600 transition-colors">
             <div className="bg-green-700 h-full p-6 flex items-center">
               <PictureInPicture size={40}/>
             </div>
             <div className="py-6 leading-relaxed">
-              <strong className="text-1xl">Wallpapers exclusivos</strong>
-              <p className="text-sm text-gray-200 mt-2">Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina</p>
+              <strong className="text-lg md:text-1xl">Wallpapers exclusivos</strong>
+              <p className="text-xs lg:text-sm text-gray-200 mt-2">Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina</p>
             </div>
             <div className="h-full p-6 flex items-center">
               <CaretRight size={24}/>
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -7,9 +7,9 @@ export function Event() {
   const { slug } = useParams<{ slug: string }>()
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen relative">
       <Header />
-      <main className="flex flex-1">
+      <main className="flex flex-1 flex-col lg:flex-row">
         { slug ? <Video lessonSlug={slug}/> : <div className="flex-1"/> }
         <Sidebar />  
       </main>
