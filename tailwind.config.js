fe05f33bf533efb3b03ae7c02e26ee3381d7fe43
Wallysson <wlc.couto@gmail.com>
2022-06-26 22:23:28 -0300

    src/ tailwind img

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        blur: 'url(src/assets/blur-background.png)'
+        blur: 'url(/src/assets/blur-background.png)'
       },
       fontFamily: {
         sans: 'Roboto, sans-serif'
